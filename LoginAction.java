d97c3fe5fe609fbbce0ea1379cd320ea8af3518a
Serin Heo <45909171+serin9811@users.noreply.github.com>
2020-05-28 20:52:20 +0900

    Create LoginAction.java

---
+++ LoginAction.java
@@ -0,0 +1,38 @@
+package com.codechobo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginAction")
+public class LoginAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id= request.getParameter("id");
+		String pwd= request.getParameter("pwd");
+		
+		//1.아이디와 비밀번호가 일치하는 지 확인한다.
+		//1.1아이디와 비밀번호가 일치한다면 
+		if(id.equals("s")&& pwd.equals("123")) {
+			//1.1.1 세션을 만들어
+			HttpSession session = request.getSession();
+			session.setAttribute("id", "s");
+			//1.1.2 그리고 그 전 jsp로  돌려보낸다.
+			System.out.println("로긴성공~");
+			String url=(String)session.getAttribute("url");
+			response.sendRedirect(url);
+		}
+		//1.2 아이디와 비밀번호가 일치하지 않는다면 
+		else {
+			//1.2.1. 로그인 폼으로 돌려보낸다.
+			System.out.println("로긴 실패 ");
+			String redirectURL = "http://localhost/loginForm.jsp";
+			response.sendRedirect(redirectURL);
+		}
+	}
+}
