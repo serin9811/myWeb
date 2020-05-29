f33fd1182829478af4faf3312fee13d97ac63af8
Serin Heo <45909171+serin9811@users.noreply.github.com>
2020-05-29 15:18:17 +0900

    Update LoginAction.java

--- LoginAction.java
+++ LoginAction.java
@@ -22,20 +22,33 @@ public class LoginAction extends HttpServlet {
 			//1.1.1 세션을 만들어
 			HttpSession session = request.getSession();
 			session.setAttribute("id", "s");
-			//1.1.2 그리고 그 전 jsp로  돌려보낸다.
+			System.out.println("세션생성완료");
 			System.out.println("로긴성공~");
-			//전 jsp에서 세션에 저장한 url을 가져온다.
-			String url=(String)session.getAttribute("url");
-			//저장한 url이 있으면
-			if(url !=null) {
-				System.out.println(url);
-				//해당 url로 이동
-				response.sendRedirect(url);
-				
-			}
-			//저장한 url이 없으면
-			else {
-				response.sendRedirect("/index.jsp");
+			
+//			<GET 방식>			
+//			//1.1.2 그리고 그 전 경로로 보내기 위해 msg를 체크한다.
+//			String msg=request.getParameter("msg");
+//			
+//			//저장한 경로가 빈문자열(=어딘가를 통하지 않고 바로 로그인폼으로 로그인 시도한 경우)
+//			if(msg ==null || msg.equals("")) {
+//				System.out.println("url msg="+msg);
+//				response.sendRedirect("http://localhost/index.jsp");
+//			}
+//			//저장한 경로가 있으면(어딘가를 통해 로그인폼으로 로그인을 시도한 경우)
+//			else {
+//				System.out.println("저장경로가 있을 때 url msg="+msg);
+//				//해당 url로 이동
+//				response.sendRedirect(msg);
+//			}
+			
+//			<POST 방식>
+			String referer= request.getHeader("referer");
+			System.out.println(referer);
+			String req=request.getRequestURI();
+			System.out.println("requestURL="+req);
+			//통해온 경로로 보내벌이기
+			if(referer != null) {
+				response.sendRedirect(referer);
 			}
 		}
 		//1.2 아이디와 비밀번호가 일치하지 않는다면 
@@ -45,5 +58,10 @@ public class LoginAction extends HttpServlet {
 			String redirectURL = "http://localhost/loginForm.jsp";
 			response.sendRedirect(redirectURL);
 		}
+	}
+	
+	//post방식으로 값 전달
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 }
